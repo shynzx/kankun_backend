29cc95ab30b4cc2eeff65b31f73d63cda53bd96a
RobertoFgo1 <jrofunsal@gmail.com>
2025-02-20 19:23:44 -0500

    fix auth: la respuesta ahora devuelve el email. cambiado el modelo de respuesta de la ruta auth (Token) -> (auth_response)

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
 from fastapi import FastAPI
 #from .routers import reservas
 from .routers import tours
-
 from .routers import auth
 from .routers import usuarios
+
+from fastapi.middleware.cors import CORSMiddleware
+
 import uvicorn
 # Iniciando la clase FastAPI para poder crear los endpoints
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"],  # Ajusta según tu frontend
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
 @app.get('/')
 def mensaje_root():
     return {
         "msg": "El servidor esta funcionando."
     }
+
 
 #app.include_router(reservas.router)
 app.include_router(tours.router)
