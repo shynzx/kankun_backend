2e44606ec800e8e56fe404e8a3de020138f52e82
RogerSantanaSuaste <23393243@utcancun.edu.mx>
2025-02-25 08:36:45 -0500

    Arreglado conexión a base de datos rota.

--- app/models/db_connect.py
+++ app/models/db_connect.py
@@ -21,7 +21,7 @@ DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "KanKunDB")
 
 # Crear conexión con la base de datos
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:asteroide-08@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 # Crear sesión asíncrona
